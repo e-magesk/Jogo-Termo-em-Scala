48d98f89b61f8f530e14ce7e211ab931adf1ef84
e-magesk <90214759+e-magesk@users.noreply.github.com>
2023-11-29 17:02:50 -0300

    Add files via upload

--- src/main/Termo/main.scala
+++ src/main/Termo/main.scala
@@ -3,38 +3,68 @@ package Termo
 import scala.io.Source
 import java.nio.file.Paths
 import play.api.libs.json._
+import scala.util.Random
 
 object main {
 
     def main(args: Array[String]): Unit = {
-
-        // val diretorioAtual = Paths.get(".").toAbsolutePath.toString
+        
+        val diretorioAtual = Paths.get(".").toAbsolutePath.toString
 
         // // Caminho para o arquivo de palavras em português
-        // val caminhoArquivo = diretorioAtual + "/src/files/br-sem-acentos.txt"
+        val caminhoArquivo = diretorioAtual + "/src/files/br-sem-acentos.txt"
 
         // // // Lê as linhas do arquivo
-        // val linhas = Source.fromFile(caminhoArquivo, "ASCII").getLines().toList
+        val linhas = Source.fromFile(caminhoArquivo, "ASCII").getLines().toList
 
         // // // Filtra as palavras com 5 letras
-        // val palavrasCom5Letras = linhas.filter(palavra => palavra.length == 5)
+        val palavrasCom5Letras = linhas.filter(palavra => palavra.length == 5)
 
-        // // // Imprime a lista de palavras com 5 letras
-        // palavrasCom5Letras.foreach(println)
+        // Crie uma instância de Random
+        val random = new Random   
+        val palavrasEscolhidas = palavrasCom5Letras(random.nextInt(palavrasCom5Letras.length))
 
-        // URL da API pública que você deseja consultar
-        val apiUrl = "https://api.dicionario-aberto.net/near/caralho"
+        println(palavrasEscolhidas)
 
-        // Realiza a chamada HTTP e obtém a resposta como uma String
-        val response = Source.fromURL(apiUrl).mkString
+        loopComReturn()
+    }
 
-        // Converte a String de resposta para um objeto Json
-        // val jsonResponse = Json.parse(response)
+    def loopComReturn(): Unit = {
+        val palavra = "viver"
+        val RESET = "\u001B[0m"
+        val RED = "\u001B[31m"
+        val GREEN = "\u001B[32m"
+        val YELLOW = "\u001B[33m"
+        val BLUE = "\u001B[34m"
 
-        println(s"Resposta da API: $response")
-        // Manipula o objeto Json conforme necessário
-        // val value = (jsonResponse \ "key").as[String]
-        // println(s"Valor da chave 'key': $value")
+        for (i <- 1 to 5) {
+
+            // Solicita ao usuário que insira uma string
+            println("Digite uma string: ")
+
+            // Lê a string do terminal
+            val inputString = scala.io.StdIn.readLine()
+
+            if (inputString == "xx" || inputString.length > 5) {
+                return // Isso encerrará a função, saindo do loop for
+            }
+
+            for (i <- 0 to 5) {
+                val caracter = inputString.charAt(i)
+
+                if(caracter == palavra.charAt(i)){
+                    print(GREEN + inputString.charAt(i) + " " + RESET)
+                }
+                else if(palavra.contains(caracter)){
+                    print(YELLOW + inputString.charAt(i) + " " + RESET)
+                }
+                else{
+                    print(inputString.charAt(i) + " ")
+                }
+            }
+            println()
+            // Resto do código dentro do loop
+        }
     }
 }
 
