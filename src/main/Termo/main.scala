8bd7b0ae0c451ebe4f5005579957a0f7977049e1
DravenPie <vininunespuf@hotmail.com>
2023-11-30 15:57:50 -0300

    palavras geradas aleatoriamente

--- src/main/Termo/main.scala
+++ src/main/Termo/main.scala
@@ -31,40 +31,37 @@ object main {
 
         var opcoes : List[Int] = List(1, 2, 3)
 
+// ------------------------------------------- Pega palavra aleatoria
+        var listaIndices : List[Int] = List(random.nextInt(palavrasCom5Letras.length),
+                                            random.nextInt(palavrasCom5Letras.length),
+                                            random.nextInt(palavrasCom5Letras.length),
+                                            random.nextInt(palavrasCom5Letras.length))
+        var listaPalavras : List[String] = List(palavrasCom5Letras.apply(listaIndices.apply(0)),
+                                                palavrasCom5Letras.apply(listaIndices.apply(1)),
+                                                palavrasCom5Letras.apply(listaIndices.apply(2)),
+                                                palavrasCom5Letras.apply(listaIndices.apply(3)))
+        
+        println(listaPalavras)
+
+// ------------------------------------------- Pega palavra aleatoria
+
         inicializacaoTermo()
-
-// ------------------------------------------- Pega palavra aleatoria
-        var vetorPalavras = Vector()
-        for( i <- 0 to 3)
-        {
-            val a = Vector(1,2,3)
-            val b = a :+ 4
-
-
-            var indicePalavra = random.nextInt(palavrasCom5Letras.length)
-            var aux = vetorPalavras :+ palavrasCom5Letras.apply(indicePalavra)
-            vetorPalavras = aux
-            // println(palavrasCom5Letras.apply(indicePalavra))
-        }
-        println(vetorPalavras)
-
-// ------------------------------------------- Pega palavra aleatoria
 
         while (opcoes.isEmpty == false) {
             var opcaoEscolhida = opcoesJogarTermo(opcoes)
 
             opcaoEscolhida match {
-            case 1 => {termoUmaPalavra("freio"); opcoes = opcoes.filter(_ != 1)}
-            case 2 => {termoDuasPalavras("mente", "areia"); opcoes = opcoes.filter(_ != 2)}
-            case 3 => {termoQuatroPalavras("mente", "areia", "aviao", "monte"); opcoes = opcoes.filter(_ != 3)}
+            case 1 => {termoUmaPalavra(listaPalavras.apply(0)); opcoes = opcoes.filter(_ != 1)}
+            case 2 => {termoDuasPalavras(listaPalavras.apply(0), listaPalavras.apply(1)); opcoes = opcoes.filter(_ != 2)}
+            case 3 => {termoQuatroPalavras(listaPalavras.apply(0), listaPalavras.apply(1), listaPalavras.apply(2), listaPalavras.apply(3)); opcoes = opcoes.filter(_ != 3)}
             case 4 => {println("\nObrigado por jogar! Até mais!\n"); return}
             }
         }
 
         Thread.sleep(2000) 
 
-        // println("Suas partidas terminaram por hoje!")
-        // println("Obrigado por jogar! Até mais!\n")
+        println("Suas partidas terminaram por hoje!")
+        println("Obrigado por jogar! Até mais!\n")
 
     }
 }
